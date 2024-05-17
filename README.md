1dab12345d04800c27310fcf53b7d162c5b597dc
David Cromianski <142181851+davidcromianski-dev@users.noreply.github.com>
2024-05-17 16:04:52 -0300

    commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<p align="center">
+  <img src="./assets/php_velho_oeste_logo.jpeg">
+</p>
---
+++ obfuscacao.php
@@ -0,0 +1,6 @@
+<?php
+
+// PHP Velho Oeste 2024
+// Palestra - Táticas de obfuscação de código em projetos PHP: Reforçando a defesa em profundidade de projetos críticos
+// Palestrante: Murilo de Araujo Chianfa
+
