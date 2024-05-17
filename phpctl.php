99c9788242c1cd58b8b940c7d5d9107b08de4a69
David Cromianski <142181851+davidcromianski-dev@users.noreply.github.com>
2024-05-17 15:58:02 -0300

    First commit

---
+++ phpctl.php
@@ -0,0 +1,28 @@
+<?php
+
+// PHP Velho Oeste - 2024
+// Palestra 1 - Aprimorando sua experiência de Desenvolvimento com Containers e a PHPCTL
+// Palestrante: Leo Cavalcante
+
+// PHPCTL não é um pacote composer, é um projeto a parte desenvolvido pelo próprio palestrante
+
+// Instalado via devContainers de opencodeco/phpctl:php83-devcontainer
+
+// - PDO MYSQL e POSTGRES
+// - MONGODB, REDIS, KAFKA
+// - Swole
+// - Decimal, DS, GD, FFI
+
+// Trocar de versao
+// - PHP_VERSION=82
+
+// PHP
+// COMPOSER
+// PHPUNIT
+// RECTOR
+//PHP-CS-FIXER
+// DEPTRAC
+// INFECTION
+// ...
+
+// Abra o arquivo .devcontainer/devcontainer.json no DevContainer
