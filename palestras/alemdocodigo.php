20ac3d854c9db13a3c356a1d09ccaecc453e978c
David Cromianski <142181851+davidcromianski-dev@users.noreply.github.com>
2024-05-17 17:44:20 -0300

    commit

--- palestras/alemdocodigo.php
+++ palestras/alemdocodigo.php
@@ -1,7 +1,12 @@
 <?php
 
-
 // PHP Velho Oeste 2024
 // Palestra 3 - Desenvolvedor além do código
 // Palestrante: Luiz Schons
 
+// Saúde do programador
+// Hobbies
+// Estudo compartilhado
+// Soft Skills
+// Gerenciamento de tempo
+// Procrastinação
---
+++ palestras/vagasphp.php
@@ -0,0 +1,7 @@
+<?php
+
+// PHP Velho Oeste 2024
+// Palestra 5 - Processos seletivos para vagas em php em tempos de crise
+// Palestrante: Sara Souza
+
+//
