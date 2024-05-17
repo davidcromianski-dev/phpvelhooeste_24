e4791b1fa889b30558322b0fb2d5d690e60010da
David Cromianski <142181851+davidcromianski-dev@users.noreply.github.com>
2024-05-17 19:26:58 -0300

    commit

---
+++ palestras/designdecodigo.php
@@ -0,0 +1,26 @@
+<?php
+
+// PHP Velho Oeste 2024
+// Palestra 6: Thinking Code: Práticas para um Design de Código de alto nível
+// Palestrante: Leonardo Tumadjian
+
+// Design: Demonstrar de cima
+
+// Desenhe antes: UML, Diagramas, Fluxogramas, etc.
+
+// Pense:
+// 1. Entender qual o problema que deve ser resolvido
+// 2. PoC ou testes de comunicação direta na API (PoC: Prova de Conceito)
+// 3. Atributos e ações
+// 4. Prognóstico, levantar a cabeça
+
+// Não use métodos estáticos
+
+// KISS my ASS -> Keep It Simple Stupid & Avoid Static Stupid
+
+// Contextos de componentes
+// 1. Cuidado com vendor locking
+// 2. Não aposte todas as fichas em apenas uma integração
+// 3. Separe por responsabilidades
+// 4. Aposte em programação genérica
+// 5. Use a OOP de verdade!
