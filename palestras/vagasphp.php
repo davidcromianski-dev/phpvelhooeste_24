6aaf76ec6b221d8f86f57906f5cff1884928bf05
David Cromianski <142181851+davidcromianski-dev@users.noreply.github.com>
2024-05-17 18:15:15 -0300

    commit

--- palestras/vagasphp.php
+++ palestras/vagasphp.php
@@ -4,4 +4,83 @@
 // Palestra 5 - Processos seletivos para vagas em php em tempos de crise
 // Palestrante: Sara Souza
 
-//
+// Bolha da internet nos anos 2000
+// A internet cresceu muito nos anos 2000, e com ela a demanda por profissionais de TI.
+// Surgiu muitas startups e investimentos desproporcionais em tecnologia.
+// No final dos anos 2000 e início dos anos 2010, a bolha estourou e muitas empresas fecharam.
+// Muitos lay-offs e demissões em massa aconteceram.
+// Consequentemente, uma baixa nas contratações de profissionais de TI.
+// Pandemia de 2020
+// A pandemia de 2020 trouxe uma crise econômica global.
+// As pessoas passaram a depender mais da tecnologia para trabalhar, estudar e se comunicar.
+// As taxas de juros baixas e a desvalorização do real fizeram com que muitas empresas investissem em tecnologia.
+// Os preços das ações de empresas de tecnologia estavam baixos
+// Alta nos números de contratações de profissionais de TI
+// Pós-pandemia 2022 - dias atuais
+// A economia está se recuperando, mas a inflação está alta.
+// Pressão dos investidores nas startups por resultado
+// redução do numero de funcionários por cortes de custos
+// Baixa nas contratações de profissionais de TI
+
+// Mudanças de exigências no mercado
+// 2020:
+// - Surgimento de startups
+// - Trabalho remoto
+// - Adaptação dos processos de recrutamento
+// 2022:
+// - Exigência de profissionais com experiência
+// - Escalabilidade
+// - Menor rodada de investimento
+
+// Processos seletivos
+// 2020: Buscavam fullstack e diferenciais
+// 2022: Muitos diferenciais viraram requisitos obrigatórios
+// Futuro: Motor de busca, elasticSearch
+
+// Vagas mais comuns: Especialistas e Sênior
+// Para liderar projetos complexos
+// Garantir a qualidade do código
+
+// Qual o papel do especialista?
+// - Liderança técnica
+// - Estratégica técnica
+// - Resolução de problemas complexos
+// - Experiencia profissional
+// - Experiencia tecnica qualitativa
+// - Comunicação
+
+// Dicas para entrevistas tecnicas
+// - Não de respostas binarias, responda de forma completa - Definição e exemplificação
+// - Direto ao ponto - Não fuja do assunto nem enrole
+// - Seja honesto - Não minta sobre suas habilidades
+// - Seja humilde - Não se ache o melhor
+// - Saiba o básico - Saiba responder respostas básicas
+// - Eu x Nós - Deixe claro sobre o que voce fez e o que foi feito em equipe
+
+// Live coding e quadros brancos
+// - Justifique sua resposta
+// - Apresente opções
+// - Escreva ou fale sobre os testes
+// - Faça da melhor forma possível
+// - Seja flexível quanto a sugestões de ferramentas
+
+// Dicas para desafios técnicos
+// - Entregas completas
+// - Atenção nas prs
+// - Sempre adicione testes
+// - Organização
+// - Avise sobre prazos
+// - Documentação
+
+// Períodos de ambientação
+// - Atenção aos padrões
+// - Aceitar os feedbacks
+// - Comunicação é a chave
+// - Atenção aos detalhes
+// - Familiarize-se com os recursos
+// - Respeito sempre
+
+// Pretenção salarial
+// - Identifique o seu nivelamento
+// - Pesquise faixas salariais no Linkedin ou GlassDoor
+// - Envie a pretensão salarial e demonstre abertura
