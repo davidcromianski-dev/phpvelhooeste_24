65d5633cbaead1a5703cdfa7ba0095b20b249c11
David Cromianski <142181851+davidcromianski-dev@users.noreply.github.com>
2024-05-18 16:49:22 -0300

    commit

--- palestras/ecossistema.php
+++ palestras/ecossistema.php
@@ -4,3 +4,5 @@
 // Palestra 11 - Ecossistema PHP: O presente, o passado e o futuro da linguagem (back-end) mais utilizada na web
 // Palestrante: Rodrigo Pokemaobr
 
+// Palestra com pouco conteúdo técnico, mais teórico sobre a história do PHP e sua evolução
+// Não anotei muito sobre
---
+++ palestras/microsservicos.php
@@ -0,0 +1,18 @@
+<?php
+
+// PHP Velho Oeste 2024
+// Palestra 12 - Microsserviços: E se falhar?
+// Palestrante: Rafael Neris
+
+// Microsserviços vs Monolito
+// - Monolito: tudo em um só lugar
+// - Microsserviços: separação de responsabilidades
+
+// Microsserviços não é a solução de tudo
+
+// Formas de comunicação
+// - Menssageria
+// - API
+// - Webhook
+// - gRPC
+
